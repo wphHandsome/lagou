ee11620db97dd1b56a807780ecf98429270d3b42
王鹏贺 <wangpenghe@WphMacBook-Pro.local>
2021-04-18 13:17:09 +0800

    作业部署

--- fed-e-task-03-03/nuxtcode/.nuxt/router.js
+++ fed-e-task-03-03/nuxtcode/.nuxt/router.js
@@ -4,7 +4,13 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _2f2cac4f = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _1b914365 = () => interopDefault(import('../pages/layout' /* webpackChunkName: "" */))
+const _7dc20fda = () => interopDefault(import('../pages/home' /* webpackChunkName: "" */))
+const _3ab833ce = () => interopDefault(import('../pages/login' /* webpackChunkName: "" */))
+const _46a148ce = () => interopDefault(import('../pages/profile' /* webpackChunkName: "" */))
+const _1ec7e19e = () => interopDefault(import('../pages/settings' /* webpackChunkName: "" */))
+const _0fc269a8 = () => interopDefault(import('../pages/editor' /* webpackChunkName: "" */))
+const _2d6b8c1b = () => interopDefault(import('../pages/article' /* webpackChunkName: "" */))
 
 const emptyFn = () => {}
 
@@ -13,14 +19,42 @@ Vue.use(Router)
 export const routerOptions = {
   mode: 'history',
   base: '/',
-  linkActiveClass: 'nuxt-link-active',
+  linkActiveClass: 'active',
   linkExactActiveClass: 'nuxt-link-exact-active',
   scrollBehavior,
 
   routes: [{
     path: "/",
-    component: _2f2cac4f,
-    name: "index"
+    component: _1b914365,
+    children: [{
+      path: "",
+      component: _7dc20fda,
+      name: "home"
+    }, {
+      path: "/login",
+      component: _3ab833ce,
+      name: "login"
+    }, {
+      path: "/register",
+      component: _3ab833ce,
+      name: "register"
+    }, {
+      path: "/profile/:username",
+      component: _46a148ce,
+      name: "profile"
+    }, {
+      path: "/settings",
+      component: _1ec7e19e,
+      name: "settings"
+    }, {
+      path: "/editor",
+      component: _0fc269a8,
+      name: "editor"
+    }, {
+      path: "/article/:slug",
+      component: _2d6b8c1b,
+      name: "article"
+    }]
   }],
 
   fallback: false
